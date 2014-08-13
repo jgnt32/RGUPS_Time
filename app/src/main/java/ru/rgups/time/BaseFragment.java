46b45752c2fd6b196b3b595aa524c63f06ccbcce
jgnt32@gmai.com <jgnt32@gmai.com>
2014-08-13 14:59:04 +0400

    holo dark icons

--- app/src/main/java/ru/rgups/time/BaseFragment.java
+++ app/src/main/java/ru/rgups/time/BaseFragment.java
@@ -6,29 +6,6 @@ import android.support.v4.app.Fragment;
 import com.octo.android.robospice.SpiceManager;
 
 public class BaseFragment extends Fragment{
-    private SpiceManager spiceManager = new SpiceManager( SampleSpiceService.class );
-    
-    
-
-    protected SpiceManager getSpiceManager() {
-        return spiceManager;
-    }
-
-
-	@Override
-	public void onStart() {
-		super.onStart();
-		spiceManager.start(getActivity());
-	}
-
-
-
-	@Override
-	public void onStop() {
-		super.onStop();
-		spiceManager.shouldStop();
-	}
-
 
 
 }
